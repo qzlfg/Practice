b39292b5dba0db9841376ef7f874de735d34c11f
Максим <maksim-turin-2007@mail.ru>
2026-06-24 17:44:37 +0600

    feat: add implementations for functions and missing tests

---
+++ task02/Class1.cs
@@ -0,0 +1,37 @@
+﻿public class Student
+{
+    public string Name { get; set; }
+    public string Faculty { get; set; }
+    public List<int> Grades { get; set; }
+}
+
+public class StudentService
+{
+    private readonly List<Student> _students;
+
+    public StudentService(List<Student> students) => _students = students;
+
+    // 1. Возвращает студентов указанного факультета
+    public IEnumerable<Student> GetStudentsByFaculty(string faculty)
+    =>  _students.Where(student => string.Equals(student.Faculty, faculty, StringComparison.OrdinalIgnoreCase));
+
+
+    // 2. Возвращает студентов со средним баллом >= minAverageGrade
+    public IEnumerable<Student> GetStudentsWithMinAverageGrade(double minAverageGrade) 
+    => _students.Where(s => s.Grades.Any() && s.Grades.Average() >= minAverageGrade);
+
+    // 3. Возвращает студентов, отсортированных по имени (A-Z)
+    public IEnumerable<Student> GetStudentsOrderedByName()
+        => _students.OrderBy(student => student.Name);
+
+    // 4. Группировка по факультету
+    public ILookup<string, Student> GroupStudentsByFaculty()
+        => _students.ToLookup(student => student.Faculty);
+
+    // 5. Находит факультет с максимальным средним баллом
+    public string GetFacultyWithHighestAverageGrade()
+        => _students
+        .GroupBy(student => student.Faculty)
+        .MaxBy(g => g.SelectMany(s => s.Grades).Any() ? g.SelectMany(s => s.Grades).Average() : 0)
+        ?.Key;
+}
