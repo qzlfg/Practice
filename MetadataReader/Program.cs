40cb748eecacbbf27b6300e3a28ad188f18f16ed
Max <maksim-turin-2007@mail.ru>
2026-07-04 01:19:45 +0600

    feat: add tests for task

--- MetadataReader/Program.cs
+++ MetadataReader/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System.Reflection;
 
-class Program
+public class Program
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
         if (args.Length == 0)
         {
