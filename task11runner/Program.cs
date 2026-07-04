14a9246a7bd8e9c6d677ab1a0e1ce9fb564de21a
Max <maksim-turin-2007@mail.ru>
2026-07-04 19:22:19 +0600

    feat: implemented all requirements for assignment

---
+++ task11runner/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.CSharp;
+using System.CodeDom.Compiler;
+using System.Reflection;
+
+namespace task11;
+
+public class Program
+{
+    public static void Main()
+    {
+        string code = @"
+        public class Calculator: ICalculator
+        {
+            public int Add(int a, int b) => a + b;
+            public int Minus(int a, int b) => a - b;
+            public int Mul(int a, int b) => a * b;
+            public int Div(int a, int b) => a / b;
+        }";
+
+        DynamicCompiler.CompileCalculator(code);
+
+    }
+}
