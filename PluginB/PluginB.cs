b3ec545e1cb7b9610a630bd967900726d5e5b746
Max <maksim-turin-2007@mail.ru>
2026-07-04 16:31:52 +0600

    feat: all needs was completed

---
+++ PluginB/PluginB.cs
@@ -0,0 +1,9 @@
+﻿
+[PluginLoad()]
+public class PluginB: ICommand
+{
+    public void Execute()
+    {
+        Console.WriteLine("Плагин B что-то выполяет");
+    }
+}
---
+++ Shared/PluginLoadAttribute.cs
@@ -0,0 +1,10 @@
+﻿[AttributeUsage(AttributeTargets.Class)]
+public class PluginLoadAttribute: Attribute
+{
+    public string[] Dependecies { get; private set; }
+
+    public PluginLoadAttribute(params string[] dependencies)
+    {
+        Dependecies = dependencies;
+    }
+}
